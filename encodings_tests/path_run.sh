2cce4a01bd46c1188d917a3bfcdcda3001dd3d59
David Rajaratnam <daver@gemarex.com.au>
2021-03-10 21:27:26 +1100

    Some updates and bug fixes to python code

--- encodings_tests/path_run.sh
+++ encodings_tests/path_run.sh
@@ -3,13 +3,16 @@
 
 BASE="path_dl_encoding"
 INSTANCE="instance.lp"
-ASP="../encodings/task_sequencing.lp ../encodings/${BASE}.lp ${INSTANCE}"
+#ASP="../encodings/task_sequencing.lp ../encodings/${BASE}.lp ${INSTANCE}"
+ASP="../encodings/${BASE}.lp ${INSTANCE}"
 
 # To pretty print the output
 clingo-dl-facts.sh "${ASP}" | clingo-facts.sh "${BASE}_user_output.lp" -
 
 # To check the solution - make sure the plan is collision free
-clingo-dl-facts.sh "${ASP}" | clingo-facts.sh "${BASE}_walk_output.lp" -
+#clingo-dl-facts.sh "${ASP}" | clingo-facts.sh "${BASE}_walk_output.lp" -
 clingo-dl-facts.sh "${ASP}" | clingo-facts.sh  "${BASE}_walk_output.lp" - | clingo "${INSTANCE}" solution_checker.lp -
+
+
 
 
